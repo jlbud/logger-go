71bd99166191d0edd90ad790ff4171829f24283f
kevin <liuwei_Kevin@unisound.com>
2021-08-04 13:36:30 +0800

    update text cases
    update log
    
    add logger

--- example/logger_core_test.go
+++ example/logger_core_test.go
@@ -6,12 +6,12 @@ import (
 )
 
 func TestCoreLogger(t *testing.T) {
-	err := logger.Init().SetLevel("debug").SetServer("cannon").Call()
+	err := logger.Init().SetServer("cannon").SetLevel("debug").Call()
 	if err != nil {
 		t.Fatal(err)
 		return
 	}
-	logger.Debug("hello cannon")
+	logger.Infof("hello cannon")
 
 	logger.WithStr("uuid", "===cannon 123===")
 	logger.Debugf("hello %s", "cannon")
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module logger-go
 
 go 1.16
 
-require github.com/rs/zerolog v1.23.0
+require (
+	github.com/oraleval/ulog v0.0.13
+	github.com/rs/zerolog v1.23.0
+)
